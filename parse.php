c3c47be855b3b0e29c7e131f4b0ca23cb2865c43
Max Base <MaxBaseCode@Gmail.Com>
2020-07-15 05:40:22 +0430

    Update parse.php

--- parse.php
+++ parse.php
@@ -2,18 +2,21 @@
 // Max Base
 // https://github.com/BaseMax/bonbast-api
 include "NetPHP.php";
+
+// Get from webpage
 $res=get("https://www.bonbast.com/");
 $result=[];
-if(preg_match('/<td>US Dollar<\/td><td([^\>]+|)>(?<value1>[^\<]+)<\/td><td([^\>]+|)>(?<value2>[^\<]+)<\/td>/i', $res[0], $usd)) {
-	$result["usd"]=[$usd["value1"], $usd["value2"]];
+preg_match_all('/<td>([^\<]+)<\/td><td([^\>]+|)>(?<value1>[^\<]+)<\/td><td([^\>]+|)>(?<value2>[^\<]+)<\/td>/i', $res[0], $_prices);
+// print_r($_prices);
+$prices[]=[];
+foreach($_prices[1] as $i=>$name) {
+	if(trim($_prices[2][$i]) == "") {
+		continue;
+	}
+	$prices[$name]=["buy"=>$_prices["value1"][$i],"sell"=>$_prices["value2"][$i]];
 }
-if(preg_match('/<td>Euro<\/td><td([^\>]+|)>(?<value1>[^\<]+)<\/td><td([^\>]+|)>(?<value2>[^\<]+)<\/td>/i', $res[0], $euro)) {
-	$result["euro"]=[$euro["value1"], $euro["value2"]];
-}
-// ...
 
 // Display output
-print "Buy Euro: ".$result["euro"][0]."\n";
-print "Sell Euro: ".$result["euro"][0]."\n";
-print "Buy Dollar: ".$result["usd"][0]."\n";
-print "Sell Dollar: ".$result["usd"][0]."\n";
+$prices=array_filter($prices);
+print_r($prices);
+// access value and rate...
