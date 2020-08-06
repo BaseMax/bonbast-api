b470d4ec8d1d2e718d98be494edd7688df550c3e
Max Base <MaxBaseCode@Gmail.Com>
2020-08-07 02:54:38 +0430

    Adding new feature: Gold Coins

--- source/bonbast-api.php
+++ source/bonbast-api.php
@@ -3,20 +3,21 @@
  * @Name: bonbast-api
  * @Author: Max Base
  * @Repository: https://github.com/BaseMax/bonbast-api
- * @Date: Jul 15, 2020
+ * @Date: Jul 15, 2020, 2020-08-07
  */
 include "NetPHP.php";
 function bonbast() {
 	// Get from webpage
 	$res=get("https://www.bonbast.com/");
 	$result=[];
-	preg_match_all('/<td>([^\<]+)<\/td><td([^\>]+|)>(?<value1>[^\<]+)<\/td><td([^\>]+|)>(?<value2>[^\<]+)<\/td>/i', $res[0], $_prices);
+	$regex='/<td>(<img class="gold-logo" style=\"([^\"]+)\" alt=\"(?<alt>[^\"]+)\"([^\>]+)>(<span style="font-size:18px;">(?<gold_prefix>[^\<]+)<\/span>|)|)(?<name>[^\<]+)<\/td><td([^\>]+|)>(?<value1>[^\<]+)<\/td><td([^\>]+|)>(?<value2>[^\<]+)<\/td>/i';
+	preg_match_all($regex, $res[0], $_prices);
 	// print_r($_prices);
 	$prices[]=[];
-	foreach($_prices[1] as $i=>$name) {
-		if(trim($_prices[2][$i]) == "") {
-			continue;
-		}
+	foreach($_prices["name"] as $i=>$name) {
+		// if(trim($_prices[6][$i]) == "") { continue; }
+		$prefix=html_entity_decode($_prices["gold_prefix"][$i]);
+		if($prefix!=="") { $name=$prefix.$name; }
 		$prices[$name]=["buy"=>$_prices["value1"][$i],"sell"=>$_prices["value2"][$i]];
 	}
 	// Filter empty value in array, we can improve code to avoid this...
